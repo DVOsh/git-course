bf68c038966d6cf6ba4e12d7a4571101ff02d711
DVOsh <oshur96@gmail.com>
2022-01-23 23:08:31 +0300

    changed log

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ const helloEl = document.createElement('h2');
 helloEl.innerText = 'Hello from JavaScript!';
 document.body.append(helloEl);
 
-console.log('Test 2');
+console.log('Test !!!!!!!!!!!!!!!!!!');
